594281be5b5c8874b0596e37a370c729c2436588
sanshi <zhz961101@gmail.com>
2018-12-08 06:52:15 +0800

    ric polyfill

--- prepare/time-slice/frame_polyfill.js
+++ prepare/time-slice/frame_polyfill.js
@@ -2,16 +2,16 @@ function poly_RAF(_easy_) {
     _easy_ = _easy_ || False
     if (_easy_) {
         // easy mode
-        return fn => setTimeout(fn,/* 1000/60 */ 17)
+        return fn => setTimeout(fn, /* 1000/60 */ 16.7)
     } else {
         // strict mode
-        function frame(callback) {
+        return function frame(fn) {
             var lastTime = 0;
-            return function(callback) {
+            return function(fn) {
                 var currTime = new Date().getTime();
                 var timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
-                var id = window.setTimeout(function() {
-                    callback(currTime + timeToCall);
+                var id = setTimeout(function() {
+                    fn(currTime + timeToCall);
                 }, timeToCall);
                 lastTime = currTime + timeToCall;
                 return id;
@@ -33,4 +33,23 @@ window.cancelFrame = (function() {
         window.mozCancelAnimationFrame ||
         window.mozCancelRequestAnimationFrame ||
         id => clearTimeout(id)
+})();
+
+// idle frame
+window.requestIdleFrame = (function() {
+    return window.requestIdleCallback || function(cb) {
+        var start = Date.now();
+        return setTimeout(function() {
+            cb({
+                didTimeout: false,
+                timeRemaining: function() {
+                    return Math.max(0, 33 - (Date.now() - start));
+                },
+            });
+        }, 1);
+    };
+})();
+
+window.cancelIdleFrame = (function() {
+    return window.cancelIdleCallback || id => clearTimeout(id);
 })();
